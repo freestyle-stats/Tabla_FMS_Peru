bcc26eda710ea832481e2bd7c8fbdff5508f5a8f
renato <renato.cabanillas@ferreyros.com.pe>
2021-07-30 00:32:24 -0500

    Tabla por jurado lista, estilo en proceso

--- dash_tabla_peru.py
+++ dash_tabla_peru.py
@@ -13,10 +13,53 @@ import xlsxwriter
 #Obtener dataset de votos de jurados
 df=pd.read_csv("FMS_PE_VotosJurados_Temporada_2.csv",delimiter=";")
 
+#Función de generar tabla
+def generate_table(dataframe, max_rows=10):
+    return html.Table(
+        # Header
+        [html.Tr([html.Th(col) for col in dataframe.columns])] +
+
+        # Body
+        [html.Tr([
+            html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
+        ]) for i in range(min(len(dataframe), max_rows))]
+    )
+
 #Declarar app y definir estilo moderno
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets,prevent_initial_callbacks=True)
 
-app.layout = html.Div([
+colors = {
+    'background': '#111111',
+    'text': '#ED514C'
+}
 
-])
+app.layout = html.Div(children=[
+    html.Div([html.H4(children='TABLA DE POSICIONES - FMS Perú',style={
+        #'textAlign':'center',
+        'color':colors['text']
+    }),
+              dcc.Dropdown(id='dropdown',options=[
+                  {'label': i, 'value': i} for i in df.Jurado.unique()
+                  ],placeholder='Elegir jurado'),
+              html.Div([html.H4(children='Tabla por jurado'),
+                        html.Div(id='tabla-jurados',
+                                 style={'width':'50%','display': 'inline-block'})
+                        ])
+              ])
+    ])
+
+@app.callback(
+    dash.dependencies.Output('tabla-jurados', 'children'),
+    [dash.dependencies.Input('dropdown', 'value')])
+def display_table(dropdown_value):
+
+    df_res = df[df.Jurado == dropdown_value]
+    df_res = df_res.groupby(['Competidor'], as_index=False).sum()
+    df_res = df_res.drop('Jornada', axis=1)
+    df_res = df_res.sort_values(['Puntos', 'Score'], ascending=False)
+    return generate_table(df_res)
+
+
+if __name__ == '__main__':
+    app.run_server()#debug=True)
